644648476f1dff819df6be0c19b7cb9748a14c7f
hewabadd <hewabadd@OA-S141-22>
2026-03-11 15:20:49 -0400

    Add project files.

---
+++ Assets/Scripts/ChangeColorAT.cs
@@ -0,0 +1,32 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class ChangeColorAT : ActionTask {
+
+		protected override string OnInit() {
+			return null;
+		}
+
+		protected override void OnExecute() {
+
+		}
+
+
+		protected override void OnUpdate() {
+			
+		}
+
+
+		protected override void OnStop() {
+			
+		}
+
+
+		protected override void OnPause() {
+			
+		}
+	}
+}
