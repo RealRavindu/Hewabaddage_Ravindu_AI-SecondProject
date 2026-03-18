9e2e1ac0c73f9e4b60778a6dbf189daa14e1a92d
hewabadd <hewabadd@OA-S141-22>
2026-03-18 18:04:21 -0400

    edited garbage spawner script a little

--- Assets/Exercise 9/Scripts/GarbageSpawner.cs
+++ Assets/Exercise 9/Scripts/GarbageSpawner.cs
@@ -4,21 +4,28 @@ public class GarbageSpawner : MonoBehaviour
 {
     public GameObject garbagePrefab;
     public KeyCode keyToSpawnGarbage;
-    [SerializeField] Vector2 xCoordBounds, yCoordBounds;
+    [SerializeField] Vector2 xCoordBounds, zCoordBounds;
+    [SerializeField] LayerMask groundLayer;
     void Update()
     {
         if (Input.GetKeyDown(keyToSpawnGarbage))
         {
+            bool coordsFound = false;
             //choose 2 random coords within 2 bounds
-            ChooseRandomCoords();
+            while (!coordsFound)
+            {
+                Vector3 coords = ChooseRandomCoords();
+                RaycastHit hit;
+                Collision[] collisions = Physics.Raycast(coords, Vector3.down, 10, groundLayer, out hit);
+            }
             //raycast to check whether its over the floor
             //if yes spawn, if not choose 2 random coords again and re-try.
 
         }
     }
 
-    Vector2 ChooseRandomCoords()
+    Vector3 ChooseRandomCoords()
     {
-        return new Vector2(Random.Range(xCoordBounds.x, xCoordBounds.y), Random.Range(yCoordBounds.x, yCoordBounds.y));
+        return new Vector3(Random.Range(xCoordBounds.x, xCoordBounds.y),10, Random.Range(zCoordBounds.x, zCoordBounds.y));
     }
 }
