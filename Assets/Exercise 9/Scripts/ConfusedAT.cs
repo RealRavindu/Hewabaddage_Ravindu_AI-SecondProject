47da2eac84801c5f8698ba4b5b7cec2f6d18b019
hewabadd <hewabadd@OA-S318-14>
2026-03-21 22:35:27 -0400

    implemented question mark appearing when garbageman couldnt find the garbage

--- Assets/Exercise 9/Scripts/ConfusedAT.cs
+++ Assets/Exercise 9/Scripts/ConfusedAT.cs
@@ -2,17 +2,21 @@ using NodeCanvas.Framework;
 using ParadoxNotion.Design;
 using UnityEngine;
 using System.Collections;
+using ParadoxNotion.Serialization.FullSerializer;
 
 namespace NodeCanvas.Tasks.Actions {
 
 	public class ConfusedAT : ActionTask {
 
+		public Transform garbageMan;
+		public Transform questionMark;
 		protected override string OnInit() {
 			return null;
 		}
 
 		protected override void OnExecute() {
 			Debug.Log("WHERE IS THE TRAAAAAAAAAAAAAAAAAAAAAAAAAAAAAASH?!?!??!");
+			StartCoroutine(confusedCR());
 			EndAction(true);
 		}
 
@@ -28,14 +32,21 @@ namespace NodeCanvas.Tasks.Actions {
 			
 		}
 
-		IEnumerator confusedCR()
+		private IEnumerator confusedCR()
 		{
 			float t = 0;
+			//Vector3 directionToCam = (Camera.main.transform.position - garbageMan.position).normalized;
+			Vector3 position = Camera.main.WorldToScreenPoint(garbageMan.position);
+			questionMark.position = position;
+			questionMark.gameObject.SetActive(true);
+			Debug.Log("Courotuine runing: " + t);
 			while(t < 1)
 			{
 				t += Time.deltaTime;
+
 				yield return null;
 			}
-		}
+            questionMark.gameObject.SetActive(false);
+        }
 	}
 }
