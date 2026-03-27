9f72750d41b68af8be71bb5966bda0e434f2262c
hewabadd <hewabadd@OA-S318-15>
2026-03-26 23:38:08 -0400

    Implemented basic auto attacking script with charge up

---
+++ Assets/Projects/ASGNMNT 2/Scripts/AutoAttackAT.cs
@@ -0,0 +1,50 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using System;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions
+{
+
+    public class AutoAttackAT : ActionTask
+    {
+        public BBParameter<GameObject> target;
+        public BBParameter<Transform> AATransform;
+        private Vector3 startPos;
+        public float timeToReachTarget;
+        private float time;
+
+        protected override string OnInit()
+        {
+            return null;
+        }
+
+        protected override void OnExecute()
+        {
+            startPos = AATransform.value.position;
+        }
+
+        protected override void OnUpdate()
+        {
+            time += Time.deltaTime / timeToReachTarget;
+            AATransform.value.position = Vector3.Lerp(startPos, target.value.transform.position, time);
+            if (time > 1)
+            {
+                GameObject.Destroy(AATransform.value.gameObject);
+                EndAction(true);
+            }
+        }
+
+
+        protected override void OnStop()
+        {
+
+        }
+
+        protected override void OnPause()
+        {
+
+        }
+    }
+}
