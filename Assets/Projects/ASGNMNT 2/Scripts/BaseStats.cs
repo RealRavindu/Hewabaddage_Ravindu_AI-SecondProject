58ff067ce9e37b302b922ddf9c326f7e2df6b394
ravin <hewabadd@sheridancollege.ca>
2026-04-04 00:08:07 -0400

    Implemented minion spawner. Implemented getting and setting for team float that auto sets the right color material and layer according  to the team number.

--- Assets/Projects/ASGNMNT 2/Scripts/BaseStats.cs
+++ Assets/Projects/ASGNMNT 2/Scripts/BaseStats.cs
@@ -2,11 +2,25 @@ using UnityEngine;
 
 public class BaseStats : MonoBehaviour
 {
-    public float team;
+    public float team
+    {
+        get
+        {
+            return _team;
+        }
+        set
+        {
+            _team = value;
+            gameObject.layer = LayerMask.NameToLayer((value == 0) ? "BlueTeam" : "RedTeam");
+            GetComponent<MeshRenderer>().material = (value == 0) ? blueMat : redMat;
+        }
+    }
+    private float _team;
     public float health;
     public float attackSpeed;
     public float range;
     public float attackDamage;
     public float abilityPower;
     public float moveSpeed;
+    public Material blueMat, redMat;
 }
---
+++ Assets/Projects/ASGNMNT 2/Scripts/MinionSpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+
+public class MinionSpawner : MonoBehaviour
+{
+    public GameObject minionPrefab;
+    public float team;
+    public float waveSpawnTime;
+    public float numberOfMinions;
+    public float spawnInterval;
+    private float timePassed;
+    private Coroutine waveSpawningCR;
+    private void Update()
+    {
+        Debug.Log(timePassed);
+        if (timePassed == 0 )
+        {
+            Debug.Log("Wave spawning CR: " +waveSpawningCR);
+            //spawn minions
+            if(waveSpawningCR == null) waveSpawningCR = StartCoroutine(spawnWave());
+
+        }
+        timePassed += Time.deltaTime;
+        if (timePassed > waveSpawnTime) timePassed = 0;
+    }
+
+    private IEnumerator spawnWave()
+    {
+        Debug.Log("Spawning wave, team: " + team);
+        int numberSpawned = 0;
+        while(numberSpawned < numberOfMinions)
+        {
+            GameObject spawnedMinion = Instantiate(minionPrefab);
+            spawnedMinion.GetComponent<BaseStats>().team = team;
+            spawnedMinion.transform.position = transform.position;
+            numberSpawned++;
+            yield return new WaitForSeconds(spawnInterval);
+        }
+
+    }
+}
